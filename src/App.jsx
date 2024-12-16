0339ce67140c0b2dd6e9782b46273716c6acd13b
Emilybrown245 <emilybrown@Emilys-MacBook-Pro.local>
2024-12-16 14:33:02 +0000

    adding nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './Components/Header'
+import NavBar from './Components/NavBar'
 
 function App() {
  return (
+  <>
+  <NavBar/>
   <Header/>
+  </>
  )
 }
 
