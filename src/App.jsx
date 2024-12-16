098ede42eed36b61cd7c14ef206bbf17b09efe84
Emilybrown245 <emilybrown@Emilys-MacBook-Pro.local>
2024-12-16 16:19:14 +0000

    list all articles

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import { useState } from 'react'
 import './App.css'
+import {Routes, Route} from 'react-router'
 import Header from './Components/Header'
 import NavBar from './Components/NavBar'
+import ArticleList from './Components/ArticleList'
+import ArticlePage from './Components/ArticlePage'
 
 function App() {
+  const [articles, setArticles] = useState([]);
  return (
   <>
   <NavBar/>
   <Header/>
+  <Routes>
+    <Route path="/" element={<ArticleList articles={articles} setArticles={setArticles}/>}></Route>
+    <Route path="/articles/:article_id" element={<ArticlePage/>}></Route>
+  </Routes>
   </>
  )
 }
---
+++ src/Components/ArticleCard.jsx
@@ -0,0 +1,22 @@
+import Card from 'react-bootstrap/Card'
+import {useParams} from 'react-router'
+import {Link} from 'react-router'
+
+function ArticleCard ({article}) {
+    
+return (
+    <div className="card-container">
+        <Card style={{ width: "10rem" }} className="card-body" >
+        <Card.Title>{article.title}</Card.Title>
+        <Card.Text>{article.author}</Card.Text>
+        <img src={article.article_img_url} className="article-image"/>
+        <Card.Text>{article.created_at}</Card.Text>
+        <Card.Text>{article.topic}</Card.Text>
+        <Link to={`articles/${article.article_id}`}><button type="submit">Read More</button></Link>
+        </Card >
+    </div>
+
+)
+}
+
+export default ArticleCard
