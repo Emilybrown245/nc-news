f6b1d404e86f54255d6ac3f57c005e0eadecf529
Emilybrown245 <emilybrown@Emilys-MacBook-Pro.local>
2024-12-20 17:01:18 +0000

    sort articles

--- src/api.js
+++ src/api.js
@@ -2,8 +2,22 @@ import axios from 'axios'
 
 const BASEURL = 'https://nc-news-lo7q.onrender.com/api';
 
-export const getArticles = async (topic) => {
-const url = topic ? `${BASEURL}/articles?topic=${topic}` : `${BASEURL}/articles`
-const {data} = await axios.get(url)
-return data.articles;
+export const getArticles = async (topic, sort_by, order) => {
+    let url = `${BASEURL}/articles`
+
+const params = [];
+
+if (topic) params.push(`topic=${topic}`);
+if (sort_by) params.push(`sort_by=${sort_by}`);
+if (order) params.push(`order=${order}`);
+if (params.length > 0) url += `?${params.join('&')}`;
+
+
+    const {data} = await axios.get(url)
+  
+    return Array.isArray(data.articles) ? data.articles : [];
+
+
 }
+
+
