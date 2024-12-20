88d7bb15c175abeea431b75761fbde801fef0b40
Emilybrown245 <emilybrown@Emilys-MacBook-Pro.local>
2024-12-20 10:18:31 +0000

    sorting articles by topic

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,11 +1,28 @@
-function NavBar () {
-    return(
-        <form>
-            <label htmlFor="topic-search">Search articles by topic: </label>
-            <input type="text" id="topic-search" name="topic" placeholder="topic"></input>
-            <button type="submit">Search</button>
-        </form>
+import {useNavigate} from 'react-router-dom'
 
+function NavBar () {
+
+  const navigate = useNavigate();
+
+    const handleChange = (e) => {
+      const selectedTopic = e.target.value
+      if(selectedTopic !== "Select" && ["coding", "football", "cooking"].includes(selectedTopic)){
+        navigate(`/topics/${selectedTopic}`);
+      } else if(selectedTopic === "Select") {
+        navigate("/");
+      }
+    }
+
+    return(
+<div>
+    <label htmlFor="dropdown">Select a topic: </label>
+      <select id="dropdown" defaultValue="Select" onChange={handleChange}>
+        <option>Select</option>
+        <option>coding</option>
+        <option>football</option>
+        <option>cooking</option>
+      </select>
+</div>
         )
 }
 
--- src/api.js
+++ src/api.js
@@ -1,1 +1,9 @@
+import axios from 'axios'
 
+const BASEURL = 'https://nc-news-lo7q.onrender.com/api';
+
+export const getArticles = async (topic) => {
+const url = topic ? `${BASEURL}/articles?topic=${topic}` : `${BASEURL}/articles`
+const {data} = await axios.get(url)
+return data.articles;
+}
