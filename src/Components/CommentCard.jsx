baec4cad607dbd10e2b177be8a907e43bce21cf7
Emilybrown245 <emilybrown@Emilys-MacBook-Pro.local>
2025-01-05 19:50:12 +0000

    making comment section responsive

--- src/Components/CommentCard.jsx
+++ src/Components/CommentCard.jsx
@@ -7,7 +7,9 @@ function CommentCard ({comment, user}) {
 return (
     <div className="comment-item">
         <img src={user.avatar_url}></img>
+        <div className='comment-body-background'>
     <p className="comment-body-article-page">{comment.body}</p>
+    </div>
     <p>{formattedDate}</p>
     <p>{comment.author}</p>
     <p>Votes: {comment.votes}</p>
